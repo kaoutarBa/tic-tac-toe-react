01e730f953f47dd9acc8da5c7d56d5894dd46653
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2021-10-23 19:33:49 +0100

    reducer

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
 import Board from "./Board";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "MOVE":
+      return {
+        ...state,
+        history: state.history.concat({ squares: action.payload.squares }),
+        xIsNext: !state.xIsNext,
+      };
+  }
+};
+
 export default function Game() {
+  const [state, dispatch] = useReducer(reducer, {
+    xIsNext: true,
+    history: [{ squares: Array(9).fill(null) }],
+  });
+  const { xIsNext, history } = state;
+
+  const handleClick = (i) => {
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    const winner = calulateWinner(squares);
+    if (winner || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+  };
+
+  const current = history[history.length - 1];
+  const winner = calulateWinner(current.squares);
   const status = "Next player is X";
   const moves = (
     <li>
